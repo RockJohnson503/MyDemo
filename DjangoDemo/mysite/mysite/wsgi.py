c7931db66f30b907b7cb990694e0f98e1bc3861a
RockJohnson503 <836867547@qq.com>
2019-04-28 11:42:18 +0800

    .

--- DjangoDemo/mysite/mysite/wsgi.py
+++ DjangoDemo/mysite/mysite/wsgi.py
@@ -7,9 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, PROJECT_DIR)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
