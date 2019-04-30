e763cb8561e312b84706957afb4a50e00a1f45ec
RockJohnson503 <836867547@qq.com>
2019-04-30 15:21:22 +0800

    .

--- DjangoDemo/introduction/blog/tests.py
+++ DjangoDemo/introduction/blog/tests.py
@@ -1,3 +1,121 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Article
+
+def create_article(title, brief_content, content):
+    return Article.objects.create(title=title, brief_content=brief_content, content=content)
+
+class ArticleContentViewTests(TestCase):
+    def test_no_content(self):
+        response = self.client.get(reverse('blog:content'))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['article_main'], [])
+        self.assertQuerysetEqual(response.context['article_new'], [])
+        self.assertQuerysetEqual(response.context['page_num'], ['1'])
+        self.assertIs(response.context['curr_page'], 1)
+        self.assertIs(response.context['next_page'], None)
+        self.assertIs(response.context['prev_page'], None)
+
+    def test_one_content(self):
+        create_article('Test', 'Test content', 'Test content')
+        response = self.client.get(reverse('blog:content'))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['article_main'], ['<Article: Test>'])
+        self.assertQuerysetEqual(response.context['article_new'], ['<Article: Test>'])
+        self.assertQuerysetEqual(response.context['page_num'], ['1'])
+        self.assertIs(response.context['curr_page'], 1)
+        self.assertIs(response.context['next_page'], None)
+        self.assertIs(response.context['prev_page'], None)
+
+    def test_more_content(self):
+        for i in range(3):
+            create_article('Test %s' % i, 'Test content %s' % i, 'Test content %s' % i)
+        response = self.client.get(reverse('blog:content'))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['article_main'], ['<Article: Test 2>', '<Article: Test 1>', '<Article: Test 0>'])
+        self.assertQuerysetEqual(response.context['article_new'], ['<Article: Test 2>', '<Article: Test 1>', '<Article: Test 0>'])
+        self.assertQuerysetEqual(response.context['page_num'], ['1'])
+        self.assertIs(response.context['curr_page'], 1)
+        self.assertIs(response.context['next_page'], None)
+        self.assertIs(response.context['prev_page'], None)
+
+    def test_first_page(self):
+        for i in range(10):
+            create_article('Test %s' % i, 'Test content %s' % i, 'Test content %s' % i)
+        response = self.client.get(reverse('blog:content') + '?page=1')
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['page_num'], ['1', '2'])
+        self.assertIs(response.context['curr_page'], 1)
+        self.assertIs(response.context['next_page'], 2)
+        self.assertIs(response.context['prev_page'], None)
+
+    def test_last_page(self):
+        for i in range(10):
+            create_article('Test %s' % i, 'Test content %s' % i, 'Test content %s' % i)
+        response = self.client.get(reverse('blog:content') + '?page=2')
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['page_num'], ['1', '2'])
+        self.assertIs(response.context['curr_page'], 2)
+        self.assertIs(response.context['next_page'], None)
+        self.assertIs(response.context['prev_page'], 1)
+
+    def test_normal_page(self):
+        for i in range(16):
+            create_article('Test %s' % i, 'Test content %s' % i, 'Test content %s' % i)
+        response = self.client.get(reverse('blog:content') + '?page=2')
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['page_num'], ['1', '2', '3', '4'])
+        self.assertIs(response.context['curr_page'], 2)
+        self.assertIs(response.context['next_page'], 3)
+        self.assertIs(response.context['prev_page'], 1)
+
+
+class ArticleDetailViewTests(TestCase):
+    def test_no_detail(self):
+        response = self.client.get(reverse('blog:detail', args=(1,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_one_detail(self):
+        a = create_article('Test', 'Test content', 'Test content')
+        response = self.client.get(reverse('blog:detail', args=(a.id,)))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual([response.context['article']], ['<Article: Test>'])
+        self.assertEqual(response.context['article_prev'], None)
+        self.assertEqual(response.context['article_next'], None)
+
+    def test_normal_detail(self):
+        for i in range(10):
+            if i == 4:
+                a = create_article('Test %s' % i, 'Test content %s' % i, 'Test content %s' % i)
+            else:
+                create_article('Test %s' % i, 'Test content %s' % i, 'Test content %s' % i)
+        response = self.client.get(reverse('blog:detail', args=(a.id,)))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual([response.context['article']], ['<Article: Test 4>'])
+        self.assertQuerysetEqual([response.context['article_prev']], ['<Article: Test 3>'])
+        self.assertQuerysetEqual([response.context['article_next']], ['<Article: Test 5>'])
+
+    def test_firt_detail(self):
+        for i in range(10):
+            if i == 9:
+                a = create_article('Test %s' % i, 'Test content %s' % i, 'Test content %s' % i)
+            else:
+                create_article('Test %s' % i, 'Test content %s' % i, 'Test content %s' % i)
+        response = self.client.get(reverse('blog:detail', args=(a.id,)))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual([response.context['article']], ['<Article: Test 9>'])
+        self.assertQuerysetEqual([response.context['article_prev']], ['<Article: Test 8>'])
+        self.assertEqual(response.context['article_next'], None)
+
+    def test_last_detail(self):
+        for i in range(10):
+            if i == 0:
+                a = create_article('Test %s' % i, 'Test content %s' % i, 'Test content %s' % i)
+            else:
+                create_article('Test %s' % i, 'Test content %s' % i, 'Test content %s' % i)
+        response = self.client.get(reverse('blog:detail', args=(a.id,)))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual([response.context['article']], ['<Article: Test 0>'])
+        self.assertQuerysetEqual([response.context['article_next']], ['<Article: Test 1>'])
+        self.assertEqual(response.context['article_prev'], None)
--- DjangoDemo/introduction/blog/views.py
+++ DjangoDemo/introduction/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseNotFound
+from django.core.paginator import Paginator
 
 from .models import Article
 
@@ -7,12 +8,37 @@ def hello_world(request):
     return HttpResponse("hello world")
 
 def article_content(request):
-    article_main = Article.objects.all().order_by('publish_date')[:2]
-    article_new = Article.objects.all().order_by('publish_date')[:10]
+    page = request.GET.get('page') or 1
+    try:
+        page = int(page)
+    except:
+        return HttpResponseNotFound()
 
-    return render(request, 'blog/index.html', {'article_main': article_main, 'article_new': article_new})
+    article_main = Article.objects.order_by('-publish_date')
+    article_new = article_main[:10]
+    paginator = Paginator(article_main, 5)
+    page_lst = paginator.page(page)
+    next_page = None
+    prev_page = None
+
+    if page_lst.has_next():
+        next_page = page + 1
+    if page_lst.has_previous():
+        prev_page = page - 1
+
+    return render(request, 'blog/index.html', {'article_main': page_lst, 'article_new': article_new,
+                                               'page_num': range(1, paginator.num_pages + 1),
+                                               'curr_page': page, 'next_page': next_page, 'prev_page': prev_page})
 
 def article_detail(request, article_id):
-    article = Article.objects.filter(pk=article_id)[0]
+    article = get_object_or_404(Article, pk=article_id)
+    try:
+        article_next = article.get_next_by_publish_date()
+    except:
+        article_next = None
+    try:
+        article_prev = article.get_previous_by_publish_date()
+    except:
+        article_prev = None
 
-    return render(request, 'blog/detail.html', {'article': article})
+    return render(request, 'blog/detail.html', {'article': article, 'article_prev': article_prev, 'article_next': article_next})
